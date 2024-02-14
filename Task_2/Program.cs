54a496ece858e7814000b4fdc56204ea86fcc111
Karina Arifullina <karinaarifullina89@gmail.com>
2024-02-14 13:49:24 +0500

    various products

---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿int i1 = 7;
+int i2 = 5;
+int sum = i1 + i2;
+Console.WriteLine(sum);
+
+long l1 = 7;
+long sum2 = l1 + i1;
+
+
+double d1 = 5.5;
+double sum3 = d1 + i1;
+
+int product1 = i1 * i2;
+long product2 = l1 * i1;
+double product3 = i1 * l1;
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
